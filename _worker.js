24dcf4696262565dabf8c2d462d0e0a1598b6192
张喜 <zhangxi@acctrue.com>
2024-11-28 15:37:04 +0800

    更改quanx配置

--- _worker.js
+++ _worker.js
@@ -94,8 +94,6 @@ export default {
 				订阅格式 = 'singbox';
 			} else if (userAgent.includes('surge') || ( url.searchParams.has('surge') && !userAgent.includes('subconverter'))){
 				订阅格式 = 'surge';
-			} else if (userAgent.includes('quantumult%20x') || (url.searchParams.has('quanx') && !userAgent.includes('subconverter'))){
-				订阅格式 = 'quanx';
 			} else if (userAgent.includes('loon') || (url.searchParams.has('loon') && !userAgent.includes('subconverter'))){
 				订阅格式 = 'loon';
 			}
@@ -109,7 +107,6 @@ export default {
 			if (url.searchParams.has('clash')) 追加UA = 'clash';
 			else if(url.searchParams.has('singbox')) 追加UA = 'singbox';
 			else if(url.searchParams.has('surge')) 追加UA = 'surge';
-			else if(url.searchParams.has('quanx')) 追加UA = 'Quantumult%20X';
 			else if(url.searchParams.has('loon')) 追加UA = 'Loon';
 			
 			const 请求订阅响应内容 = await getSUB(urls ,request ,追加UA, userAgentHeader);
@@ -171,8 +168,6 @@ export default {
 				subconverterUrl = `${subProtocol}://${subconverter}/sub?target=singbox&url=${encodeURIComponent(订阅转换URL)}&insert=false&config=${encodeURIComponent(subconfig)}&emoji=true&list=false&tfo=false&scv=true&fdn=false&sort=false&new_name=true`;
 			} else if (订阅格式 == 'surge'){
 				subconverterUrl = `${subProtocol}://${subconverter}/sub?target=surge&url=${encodeURIComponent(订阅转换URL)}&insert=false&config=${encodeURIComponent(subconfig)}&emoji=true&list=false&tfo=false&scv=true&fdn=false&sort=false&new_name=true`;
-			} else if (订阅格式 == 'quanx'){
-				subconverterUrl = `${subProtocol}://${subconverter}/sub?target=quanx&url=${encodeURIComponent(订阅转换URL)}&insert=false&config=${encodeURIComponent(subconfig)}&emoji=true&list=false&tfo=false&scv=true&fdn=false&sort=false&udp=true`;
 			} else if (订阅格式 == 'loon'){
 				subconverterUrl = `${subProtocol}://${subconverter}/sub?target=loon&url=${encodeURIComponent(订阅转换URL)}&insert=false&config=${encodeURIComponent(subconfig)}&emoji=true&list=false&tfo=false&scv=true&fdn=false&sort=false`;
 			}
